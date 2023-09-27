7d444127704aa4a60b3a04fa9982a176756f6475
Franklin Hau <fhauchi@gmail.com>
2023-09-26 19:37:37 -0600

    starting to code models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db 
 
 from config import db
 
