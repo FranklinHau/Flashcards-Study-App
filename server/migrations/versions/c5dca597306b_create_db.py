9b8c0f5893cdbf0f3a5ca2249e9e414e00c77b61
Franklin Hau <fhauchi@gmail.com>
2023-09-26 18:59:43 -0600

    writing code for seed.py

---
+++ server/migrations/versions/c5dca597306b_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: c5dca597306b
+Revises: 
+Create Date: 2023-09-26 18:20:38.088284
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c5dca597306b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,38 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
+
+    # Initialize FLask app context
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+        
+        # Delete existing data
+        # This is to start with a clean stats 
+        db.session.query(User).delete()
+
+        # Number of users to create 
+        num_users = 50
+
+        # Create and add new users
+        for _ in range(num_users):
+            # Generate fake username and email 
+            username = fake.user_name()
+            email = fake.email()
+
+            # Create new User instance
+            # Using the fake username and email 
+            new_user = User(username=username, email=email)
+
+            # Add the new user to the database session
+            # This prepares them to be committed to the database 
+            db.session.add(new_user)
+
+        # Commit the new users to the database 
+        # Now, the database will actually be updated
+        db.session.commit()
+
+        print(f"Seeded {num_users} users!") 
