a9701e94b98371492735cd778e9212c3f523e189
Franklin Hau <fhauchi@gmail.com>
2023-09-26 19:45:33 -0600

    added hashed_password to user model

--- server/models.py
+++ server/models.py
@@ -8,4 +8,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    
+    hashed_password = db.Column(db.String(100), nullable=False)
+
