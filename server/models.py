768a76159c0a1ed1faea04ad5cfc1344e9a35895
Franklin Hau <fhauchi@gmail.com>
2023-09-26 19:43:40 -0600

    added email to user model

--- server/models.py
+++ server/models.py
@@ -7,3 +7,5 @@ from config import db
 class User(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    
