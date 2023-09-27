873150237f292c6953a8707bf1ea442ddcac0bcc
Franklin Hau <fhauchi@gmail.com>
2023-09-26 19:40:08 -0600

    added id to User model

--- server/models.py
+++ server/models.py
@@ -1,8 +1,8 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db 
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 from config import db
 
-# Models go here!
+class User(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
