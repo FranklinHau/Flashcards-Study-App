da84c29e5a1f557b6ca2cab7e83450e37e8f7e04
Franklin Hau <fhauchi@gmail.com>
2023-10-01 19:25:35 -0600

    class Deck title created

--- server/models/deck.py
+++ server/models/deck.py
@@ -3,5 +3,6 @@ from app import db
 class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     
     
