2152af8d49d6a65e02e9671efebccf7816fb3d93
Franklin Hau <fhauchi@gmail.com>
2023-10-01 19:27:31 -0600

    class Deck description created

--- server/models/deck.py
+++ server/models/deck.py
@@ -4,5 +4,7 @@ class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
     
+
     
