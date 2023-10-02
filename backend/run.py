0b427d9c1a2ef54dd73ce291dc5d77861cd7e92d
Franklin Hau <fhauchi@gmail.com>
2023-10-02 00:07:21 -0600

    registered Blueprint to run.py

--- backend/run.py
+++ backend/run.py
@@ -3,9 +3,11 @@
 from config import app
 from app.routes import deck_routes
 from app.routes import user_routes 
+from app.routes import card_routes 
     
 app.register_blueprint(deck_routes)
 app.register_blueprint(user_routes)
+app.register_blueprint(card_routes)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
