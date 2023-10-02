cc44b08ec183c39901228fe85937ff7a08f7124b
Franklin Hau <fhauchi@gmail.com>
2023-10-01 23:03:05 -0600

    register Blueprint to run.py

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,10 @@
 
 from config import app
 from app.routes import deck_routes
+from app.routes import user_routes 
     
 app.register_blueprint(deck_routes)
+app.register_blueprint(user_routes)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
