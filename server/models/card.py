5d8fce9f960f76a40c4ca09d67fc0b26b4688f2e
Franklin Hau <fhauchi@gmail.com>
2023-10-01 20:40:50 -0600

    staring tocomment on card.py

--- server/models/card.py
+++ server/models/card.py
@@ -1,5 +1,6 @@
 from config import db 
 
+# Defining the Card model
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)
