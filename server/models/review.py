fdc15aad887f5bad05874a556957ce1d2ad005e6
Franklin Hau <fhauchi@gmail.com>
2023-10-01 19:54:26 -0600

    class Review user_id column created

--- server/models/review.py
+++ server/models/review.py
@@ -3,4 +3,5 @@ from app import db
 class Review(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
