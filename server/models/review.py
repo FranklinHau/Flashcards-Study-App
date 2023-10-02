b4eee171614e547e2632fe2fcbb16d686fa14254
Franklin Hau <fhauchi@gmail.com>
2023-10-01 19:55:59 -0600

    class Review comment column created

--- server/models/review.py
+++ server/models/review.py
@@ -4,4 +4,6 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.Text)
     
