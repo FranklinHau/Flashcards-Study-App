3b184294d439f77f5327b85a6e0cdcd235f73ad1
Franklin Hau <fhauchi@gmail.com>
2023-10-02 19:59:09 -0600

    inserted code to create a new review for review_routes.py

--- backend/app/routes/review_routes.py
+++ backend/app/routes/review_routes.py
@@ -2,3 +2,20 @@ from flask import Blueprint, jsonify, request
 from config import db 
 from app.models.review import Review
 
+#create a Blueprint for the review routes 
+review_routes = Blueprint('review_routes', __name__)
+
+# create a new review
+@review_routes.route('/api/review', methods=['POST'])
+def create_review():
+    data = request.get_json()
+    new_review = Review(
+        deck_id=data['deck_id'], 
+        user_id=data['user_id'],
+        rating=data.get('rating'), # rating is optional 
+        comment=data.get('comment') # comment is optional 
+    )
+    db.session.add(new_review)
+    db.session.commit()
+    return jsonify({'message': 'New review created'}), 201
+
