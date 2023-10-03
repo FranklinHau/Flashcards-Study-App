3d0aab1f04ecebf274ddfd6adf29f54cc480b7ce
Franklin Hau <fhauchi@gmail.com>
2023-10-02 22:40:10 -0600

    change code from App.js to UserComponent

---
+++ client/src/components/UserComponent.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserComponent = () => {
+  // Initilize user state 
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({ username: '', email: '', password: ''});
+
+  // Load existing users when components mounts 
+  useEffect(() => {
+    axios.get('/users')
+      .then(responde => {
+        setUsers(response.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+  // Function to create a new user 
+  const createUser = () => {
+    axios.post('/users', newUser)
+    .then(response => {
+      // update user state with new user
+      setUsers([...users, response.data]);
+    })
+    .catch(error => console.log(error));
+  };
+  // function to delete a user 
+  const deleteUser = (id) => {
+    axios.delete(`/users/${id}`)
+      .then(response => {
+        // remove deleted user from state)
+        setUsers(users.filter(user => user.id !== id));
+  })
+    .catch(error => console.log(error));
+  };
+
+  // function to update a user 
+  const updateUser = (id, updateUser) => {
+    axios.put(`/users/${id}`, updateUser)
+      .then(response => {
+        // update state with updated user 
+        setUsers(user.map(user => (user.id == id ? updateUser : user)));
+      })
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      {/* Display existing users */}
+      <ul>
+        {users.map(user => (
+          <li key={user.id}> 
+            {user.username} - {user.email}
+            <button onClick={() => deleteUser(user.id)}>Delete</button>
+            </li>
+        ))}
+      </ul>
+
+      {/* Form to create a new user */}
+      <input
+        type='text'
+        placeholder='Username'
+        value={newUser.username}
+        onChange={e => setNewUser({ ...newUser, username: e.target.value})}
+      />
+      <input
+        type='email'
+        placeholder='Email'
+        value={newUser.email}
+        OnChange={e => setNewUser({ ...newUser, email: e.target.value })}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={newUser.password}
+        onChange={e => setNewUser({ ...newUser, password: e.target.value })}
+      />
+      <button onClick={createUser}>Create User</button>
+    </div>
+  );
+};
+
+export default UserComponent; 
