d143bb3bd144ca274ce46287a1b1a4771a7ed997
Franklin Hau <fhauchi@gmail.com>
2023-10-02 21:56:50 -0600

    created const UserComponent

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const UserComponent = () => {
+  // Initilize user state 
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({ username: '', email: '', password: ''});
 
+  // Load existing users when components mounts 
+  useEffect(() => {
+    axios.get('/users')
+      .then(responde => {
+        setUsers(response.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+}
