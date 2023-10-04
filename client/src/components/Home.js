9d52941feefc80133737ca0a70ff8c4fbf4ffc13
Franklin Hau <fhauchi@gmail.com>
2023-10-03 20:50:25 -0600

    code for sample data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,8 @@
 import React from 'react';
 
 // sample data representing public decks 
-const publicDecks = []
+const publicDecks = [
+    {id: 1, title: 'Science Deck', description: 'Learn abput basic science.'},
+    {id: 2, title: 'History Deck', description: 'Learn about world history.'}
+];
+
