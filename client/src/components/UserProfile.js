22325b6101e2a74861d79739192a1b5022f6574e
Franklin Hau <fhauchi@gmail.com>
2023-10-03 21:28:07 -0600

    inserted code for UserProfile.js

---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+function UserProfile() {
+    // Initialize state for user and decks 
+    const [user, setUser] = useState({});
+    const [decks, setDecks] = useState([]);
+}
+
+return (
+    <div className='user-profile-container'>
+        <h1>{user.username}'s Profile</h1>
+        <p>Email: {user.email}</p>
+        <p>Bio: {user.bio}</p>
+
+        <h2>Your Decks</h2>
+        <ul>
+            {decks.map((deck) => (
+                <li key={deck.id}>
+                    <div>Title: {deck.title}</div>
+                    <div>Description: {deck.description}</div>
+                </li>
+            ))}
+        </ul>
+    </div>
+)
