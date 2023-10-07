3ffc4922abc4c66bce7021f4f61a3273ae83c5f0
Franklin Hau <fhauchi@gmail.com>
2023-10-07 16:32:28 -0600

    trying to modify my home page

--- backend/run.py
+++ backend/run.py
@@ -12,5 +12,9 @@ app.register_blueprint(user_routes)
 app.register_blueprint(card_routes)
 app.register_blueprint(review_routes)
 
+@app.route('/')
+def home():
+    return 'Hello, this is the home page'
+
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5555)), debug=True)
 
