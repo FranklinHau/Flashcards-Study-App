d3f69c319d42d6470e520c602edecff711736e2b
Franklin Hau <fhauchi@gmail.com>
2023-10-07 23:03:04 -0600

    deleted code to use formik & yup

--- client/src/components/UserComponent.js
+++ client/src/components/UserComponent.js
@@ -1,120 +1,104 @@
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios';
 import config from './config';
-import { ReCAPTCHA } from 'react-google-recaptcha';
-
-
 
 const UserComponent = () => {
-  // Initilize user state 
-  const [users, setUsers] = useState([]);
-  const [newUser, setNewUser] = useState({ username: '', email: '', password: '' });
-  const [message, setMessage] = useState('');
-  const [termsAccepted, setTermsAccepted] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [captchaValue, setCaptchaValue] = useState('');
-  const [isCaptchaValid, setIsCaptchaValid] = useState(false); 
-  
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      termsAccepted: false,
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(3, 'Must be at least 3 characters')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email format')
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Password must be at least 8 characters long')
+        .required('Required'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], "Passwords don't match")
+        .required('Required'),
+      termsAccepted: Yup.boolean()
+        .oneOf([true], 'You must accept the terms and conditions'),
+    }),
+    onSubmit: async (values) => {
+      // Your Axios call here
+      try {
+        await axios.post(`${config.apiBaseURL}/users`, values);
+        alert('User successfully registered');
+      } catch (error) {
+        alert('An error occurred');
+        console.log('Error', error);
+      }
+    },
+  });
 
-  // Funtion to handle CAPTCHA
-  const handleCaptcha = (value) => {
-    if (value) {
-      setIsCaptchaValid(true);
-    } else {
-      setCaptchaValue(false);
-    }
-  }
-  // Function to create a new user 
-  const createUser = async () => {
-    // validation
-    if (!newUser.email.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)) {
-      setMessage('Invalid email format');
-      return;
-    }
-    if (newUser.password.length < 8) {
-      setMessage('Password must be at least 8 characters long');
-      return;
-    }
-    if (newUser.password !== newUser.confirmPassword) {
-      setMessage("Passwords don't match");
-      return;
-    }
-    if (!termsAccepted) {
-      setMessage('You must accept the terms and conditions');
-      return;
-    }
-    if (!isCaptchaValid) {
-      setMessage('Invalid CAPTCHA'); 
-      return;
-    }
-
-    setIsLoading(true); // starts loading 
-
-    try {
-      await axios.post(`${config.apiBaseURL}/users`, newUser);
-      setMessage('User successfully registered');
-    } catch (error) {
-      setMessage('An error occurred');
-      console.log('Error', error);
-    }
-    setIsLoading(false); // stops loading 
-  };
-
-    return (
-      <div className='Registration-container'>
-        <h2>Register</h2>
-        {message && <div className='message'>{message}</div>}
-        {isLoading ? <div>Loading...</div> : null}
-
-        {/* Form to create a new user */}
+  return (
+    <div className="Registration-container">
+      <h2>Register</h2>
+      <form onSubmit={formik.handleSubmit}>
         <input
-          type='text'
-          placeholder='Username'
-          value={newUser.username}
-          onChange={e => setNewUser({ ...newUser, username: e.target.value })}
+          type="text"
+          name="username"
+          placeholder="Name"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.username}
         />
-        <input
-          type='email'
-          placeholder='Email'
-          value={newUser.email}
-          onChange={e => setNewUser({ ...newUser, email: e.target.value })}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          value={newUser.password}
-          onChange={e => setNewUser({ ...newUser, password: e.target.value })}
-        />
-        <input
-          type='password'
-          placeholder='Confirm Password'
-          value={newUser.confirmPassword}
-          onChange={e => setNewUser({ ...newUser, confirmPassword: e.target.value })}
-        />
-        <input
-          type='checkbox'
-          id='terms'
-          checked={termsAccepted}
-          onChange={e => setTermsAccepted(e.target.checked)}
-        />
-        {/* <input
-          type='text'
-          placeholder='Enter CAPTCHA'
-          value={captchaValue}
-          onChange={e => {
-            const value = e.target.value;
-            setCaptchaValue(value);
-            handleCaptcha(value);
-          }}
-          />
-          <ReCAPTCHA
-            sitekey='YOUR_RECAPTCHA_SITE_KEY_HERE'
-            onChange={handleCaptcha}
-          /> */}
-        <label htmlFor='terms'>I accept the terms and conditions</label>
-        <button onClick={createUser}>Create User</button>
-      </div>
-    );
-  };
+        {formik.touched.username && formik.errors.username ? <div>{formik.errors.username}</div> : null}
 
-  export default UserComponent;
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.confirmPassword}
+        />
+        {formik.touched.confirmPassword && formik.errors.confirmPassword ? <div>{formik.errors.confirmPassword}</div> : null}
+
+        <input
+          type="checkbox"
+          name="termsAccepted"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          checked={formik.values.termsAccepted}
+        />
+        <label htmlFor="termsAccepted">I accept the terms and conditions</label>
+        {formik.touched.termsAccepted && formik.errors.termsAccepted ? <div>{formik.errors.termsAccepted}</div> : null}
+
+        <button type="submit">Create User</button>
+      </form>
+    </div>
+  );
+};
+
+export default UserComponent;
