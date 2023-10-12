1ec5511fb6f8921357d416ebac63eac69378b9ea
Franklin Hau <fhauchi@gmail.com>
2023-10-12 02:25:29 -0600

    added comments to try to look for errors

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+// NavBar Functional Component
 function NavBar({ user, handleLogout }) {
+  // Hook for programmatic navigation
   const navigate = useNavigate();
 
+  // Function to handle logout and navigation
   const logoutAndNavigate = () => {
+    // Confirming logout action with the user
     const confirmLogout = window.confirm("You're about to be logged out from your account. Do you wish to continue?");
+    
     if (confirmLogout) {
-    handleLogout(); 
-    navigate("/login");
+      handleLogout(); // Calling logout handler passed as prop
+      navigate("/login"); // Navigating to login page
     }
   };
-
+  // Rendering the NavBar UI
   return (
     <nav>
       {user ? (
         <>
-          <button onClick={logoutAndNavigate}>Home</button>
+          <button onClick={logoutAndNavigate}>Home</button>  {/* Button to trigger logout and navigate to login page */}
         </>
       ) : (
-        <Link to="/login">Home</Link>
+        <Link to="/login">Home</Link>  //{/* Link to navigate to login page if user is not authenticated */}
       )}
     </nav>
   );
