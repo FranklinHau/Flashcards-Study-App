8393bbd10c1f5e4509be18f6ff50f13b7d220a0a
Franklin Hau <fhauchi@gmail.com>
2023-10-12 02:33:52 -0600

    add comments to store.js remember how to do it

--- client/src/components/store.js
+++ client/src/components/store.js
@@ -1,11 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import deckReducer from './deckSlice';
+// Importing necessary functions and slices from Redux and other files
+import { configureStore } from '@reduxjs/toolkit'; // Function to configure the Redux store
+import deckReducer from './deckSlice'; // Reducer function for the 'deck' slice of the state
 
-
+// Configuring and creating the Redux store
 const store = configureStore({
     reducer: {
-        deck: deckReducer
+        deck: deckReducer // Assigning the deckReducer to manage the 'deck' slice of the state
     }
 });
+
 
 export default store;
